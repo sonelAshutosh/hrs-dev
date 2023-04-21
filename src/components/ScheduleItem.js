3e26e6a01af95f7e4b944aa3d708e5f0be8cbe34
sonelAshutosh <ashutoshhoonmain@gmail.com>
2023-04-21 19:13:26 +0530

    Moved handleSchedule function to ScheduleItem.js

--- src/components/ScheduleItem.js
+++ src/components/ScheduleItem.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 import styles from '../styles/ScheduleItem.module.css'
 
 function ScheduleItem(props) {
+  const router = useRouter()
+  const handleSchedule = () => {
+    router.push('/my-schedules/schedule-1')
+  }
   return (
-    <div className={styles['schedule-item']} onClick={props.onClick}>
+    <div className={styles['schedule-item']} onClick={handleSchedule}>
       <div className={styles['schedule-item-title']}>CSE 2022 VI Sem</div>
       <div className={styles['schedule-item-code']}>CSEVI22</div>
     </div>
--- src/pages/my-schedules/index.js
+++ src/pages/my-schedules/index.js
@@ -1,23 +1,9 @@
 import ScheduleItem from '@/components/ScheduleItem'
 import styles from '../../styles/Index.module.css'
-import { useRouter } from 'next/router'
 
 export default function index() {
-  const router = useRouter()
-  const handleSchedule = () => {
-    router.push('/my-schedules/schedule-1')
-  }
   return (
     <div className={styles['my-schedules']}>
-      <ScheduleItem onClick={handleSchedule} />
-      <ScheduleItem onClick={handleSchedule} />
-      <ScheduleItem onClick={handleSchedule} />
-      <ScheduleItem onClick={handleSchedule} />
-      <ScheduleItem onClick={handleSchedule} />
-      <ScheduleItem onClick={handleSchedule} />
-      <ScheduleItem onClick={handleSchedule} />
-      <ScheduleItem onClick={handleSchedule} />
-      {/* <ScheduleItem />
       <ScheduleItem />
       <ScheduleItem />
       <ScheduleItem />
@@ -26,7 +12,6 @@ export default function index() {
       <ScheduleItem />
       <ScheduleItem />
       <ScheduleItem />
-      <ScheduleItem /> */}
     </div>
   )
 }
